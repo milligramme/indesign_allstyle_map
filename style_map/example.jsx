272442886f4397692019ba12129c612b78897c16
milligramme <milligramme.cc@gmail.com>
2014-07-04 10:44:27 +0900

    M: modify example.jsx

--- style_map/example.jsx
+++ style_map/example.jsx
@@ -1,7 +1,7 @@
 #include "./index.jsx"
 var doc = app.documents[0];
 var aps = new StyleMap(doc.allParagraphStyles);
-var cps = new StyleMap(doc.allCharacterStyles);
+var acs = new StyleMap(doc.allCharacterStyles);
 
 var u;
 var w = new Window('dialog', u, u);
@@ -10,7 +10,7 @@ w.margins = 5;
 w.spacing = 5;
 w.alignChildren = ['fill', 'fill'];
 
-w.cstyle_ddl = w.add('dropdownlist', u, cps.list);
+w.cstyle_ddl = w.add('dropdownlist', u, acs.list);
 w.pstyle_ddl = w.add('dropdownlist', u, aps.list);
 
 w.cstyle_ddl.title = "Char Style!!";
